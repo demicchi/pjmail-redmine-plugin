2867d6e233f4228d18d1b71fae525c2eb343ca27
demicchi <747487+demicchi@users.noreply.github.com>
2024-02-18 23:14:16 +0900

    add a mail address next to a pjmail url.

--- pjmail/lib/pjmail_hook_listener.rb
+++ pjmail/lib/pjmail_hook_listener.rb
@@ -7,8 +7,11 @@ class PjmailHookListener < Redmine::Hook::ViewListener
       concat(
         content_tag(:div, class: "value") do
           concat(link_to(nil, Setting.plugin_pjmail['pjmail_url'] + '?pj=' + issue.project.identifier + '&num=' + issue.id.to_s, target: '_blank', rel: 'noopener noreferrer'))
-          concat(' mail: ')
-          concat(link_to(nil, 'mailto:' + issue.project.identifier + Setting.plugin_pjmail['pjmail_separator'] + issue.id.to_s + '@' + Setting.plugin_pjmail['settings_pjmail_domain']))
+          concat(' (mail: ')
+          concat(link_to(issue.project.identifier + Setting.plugin_pjmail['pjmail_separator'] + issue.id.to_s + '@' + Setting.plugin_pjmail['pjmail_domain'],
+                         'mailto:' + issue.project.identifier + Setting.plugin_pjmail['pjmail_separator'] + issue.id.to_s + '@' + Setting.plugin_pjmail['pjmail_domain'])
+          )
+          concat(')')
         end
       )
     end
